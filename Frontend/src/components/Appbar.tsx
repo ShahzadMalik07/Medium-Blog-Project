ad605a60dfbc82f79907355574c81a5d3a0e9df3
ShahzadMalik07 <shahzadmalik425@gmail.com>
2024-10-23 23:56:40 +0530

    search bar added in header

--- Frontend/src/components/Appbar.tsx
+++ Frontend/src/components/Appbar.tsx
@@ -8,6 +8,7 @@ const Appbar = () => {
         <div className="border-b border-slate-200 flex justify-between px-8 py-3">
             <div className="flex items-center text-2xl font-medium">
                 <Link to={"/blogs"}>Medium</Link>
+                <div className="rounded-full bg-gray-300 px-2 py-1"><input className="border-none" type="text" placeholder="Search" /></div>
             </div>
             <div className="flex items-center">
                 <Link to={"/publish"}><button className="flex items-center gap-2 text-xl px-2 py-2 text-gray-700 mr-4 rounded-md"><LiaEdit className="text-3xl"/> Write</button></Link>
